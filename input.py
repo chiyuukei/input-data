0472d09299ea10fc500327d9138242d1a8711417
ゆう <247145247+chiyuukei@users.noreply.github.com>
2025-12-04 13:50:18 +0700

    Update input.py

--- input.py
+++ input.py
@@ -1,8 +1,14 @@
 import json
 import os
 
+
 def collect_data():
-    person = {"name": "age", "dob": "religion", "address": "country", "email": "phone"} # → 必要に応じてデータリストを変更します。
+    person = {
+        "name": "age",
+        "dob": "religion",
+        "address": "country",
+        "email": "phone",
+    }  # → 必要に応じてデータリストを変更します。
 
     print("\n===== ENTER DATA =====\n")
 
@@ -17,25 +23,29 @@ def collect_data():
 
     return person
 
-def load_existing_data(filename='/result/result.json'):  # → 出力フォルダー
+
+def load_existing_data(filename="/result/result.json"):  # → 出力フォルダー
     if os.path.exists(filename):
-        with open(filename, 'r') as json_file:
+        with open(filename, "r") as json_file:
             data = json.load(json_file)
             return data
     return []
 
-def save_data_to_json(data, filename='/result/result.json'):  # → 出力フォルダー
-    with open(filename, 'w') as json_file:
+
+def save_data_to_json(data, filename="/result/result.json"):  # → 出力フォルダー
+    with open(filename, "w") as json_file:
         json.dump(data, json_file, indent=1)
     print(f"\n→ Folder Location : {filename}\n")
+
 
 def main():
     existing_data = load_existing_data()
     new_data = collect_data()
-    
+
     existing_data.append(new_data)
 
     save_data_to_json(existing_data)
+
 
 if __name__ == "__main__":
     main()
